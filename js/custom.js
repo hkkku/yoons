3774b87f754a14662e57bb2dd098efb0442f7016
hkkku <hhk7045@gmail.com>
2021-03-30 17:32:35 +0900

    03/30 네비게이션 디자인 & 횡스크롤 슬라이드 기능 구현

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+var banner = document.getElementById('banner')
+var lastBanner = banner.getElementsByClassName('bannerImg');
+
+lastBanner = lastBanner[lastBanner.length-1];
+var dummyX = null;
+
+window.onscroll = function(){
+  var y = document.body.getBoundingClientRect().top;
+  banner.scrollLeft = -y;
+  
+  var diff = window.scrollY - dummyX;
+  if(diff > 0){
+    window.scrollTo(0, diff);
+  } else if(window.scrollY == 0){
+    window.scrollTo(0, dummyX);
+  };
+};
+
+  
+window.onresize = resize;
+resize();
+
+function resize(){
+  var w = banner.scrollWidth-window.innerWidth+window.innerHeight;
+  document.body.style.height = w + "px";
+
+  dummyX = lastBanner.getBoundingClientRect().left+window.scrollY;
+}
