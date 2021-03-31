47671366c6de62aeaef5cf76d45d9af14cfc9183
hkkku <hhk7045@gmail.com>
2021-03-31 18:34:54 +0900

    03/31 횡스크롤 배너 구현 완료

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,31 @@
-var banner = document.getElementById('banner')
-var lastBanner = banner.getElementsByClassName('bannerImg');
+// var banner = document.getElementById('banner')
+// var lastBanner = banner.getElementsByClassName('bannerImg');
 
-lastBanner = lastBanner[lastBanner.length-1];
-var dummyX = null;
+// lastBanner = lastBanner[lastBanner.length-1];
+// var dummyX = null;
 
-window.onscroll = function(){
-  var y = document.body.getBoundingClientRect().top;
-  banner.scrollLeft = -y;
+// window.onscroll = function(){
+//   var y = document.body.getBoundingClientRect().top;
+//   var w = banner.scrollWidth-window.innerWidth+window.innerHeight;
+//   banner.scrollLeft = -y;
+//   document.body.style.height = w + "px";
+//   dummyX = lastBanner.getBoundingClientRect().left+window.scrollY;
+
+//   var diff = window.scrollY - dummyX;
+//   if(diff > 0){
+//     window.scrollTo(0, diff);
+//   } else if(window.scrollY == 0){
+//     window.scrollTo(0, dummyX);
+//   };
+// };
+
   
-  var diff = window.scrollY - dummyX;
-  if(diff > 0){
-    window.scrollTo(0, diff);
-  } else if(window.scrollY == 0){
-    window.scrollTo(0, dummyX);
-  };
-};
+// window.onresize = resize;
+// resize();
 
-  
-window.onresize = resize;
-resize();
+// function resize(){
+//   var w = banner.scrollWidth-window.innerWidth+window.innerHeight;
+//   document.body.style.height = w + "px";
 
-function resize(){
-  var w = banner.scrollWidth-window.innerWidth+window.innerHeight;
-  document.body.style.height = w + "px";
-
-  dummyX = lastBanner.getBoundingClientRect().left+window.scrollY;
-}
+//   dummyX = lastBanner.getBoundingClientRect().left+window.scrollY;
+// }
